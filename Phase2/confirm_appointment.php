0aca08ee8e9dce050b683663690b96930bd3aca6
SAlhowaimel <162506497+SAlhowaimel@users.noreply.github.com>
2025-03-22 06:36:27 +0300

    Add files via upload

---
+++ Phase2/confirm_appointment.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include "DBconnection.php";
+
+if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== "doctor") {
+    header("Location: Ebsar Homepage.php");
+    exit();
+}
+
+$appointmentID = $_GET['id'];
+$sql = "UPDATE appointment SET status='Confirmed' WHERE id='$appointmentID'";
+mysqli_query($conn, $sql);
+
+header("Location: Doctor.php");
+exit();
+?>
