41cff1e5deb233237595f80f00a1a790d2478182
FajerAlamro <fajeralamro@gmail.com>
2025-04-02 18:26:27 +0300

    Update AddPrescribeMedication.php

--- AddPrescribeMedication.php
+++ AddPrescribeMedication.php
@@ -1,10 +1,4 @@
 <?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
  error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
